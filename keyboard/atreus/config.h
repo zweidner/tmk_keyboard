5d105924e990b13985a38bb89eda92a41304aa96
Zach Weidner <zach.weidner@gmail.com>
2015-09-16 23:39:45 -0500

    another try at teensy key

--- keyboard/atreus/config.h
+++ keyboard/atreus/config.h
@@ -18,6 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#define BOOTLOADER_SIZE     4096
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0x1337
